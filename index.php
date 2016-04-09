0e3b5212a1abee143fdc9e8308322e8c3920bb2a
Masoud A <amini141@gmail.com>
2016-04-09 10:44:45 +0430

    update

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: درگاه زرین پال گراویتی فرم
+Plugin URI: http://gravityforms.ir/
+Description: افزونه درگاه پرداخت زرین پال برای فرم ساز فوق پیشرفته Gravity Forms
+Version: 2.0.0
+Author: حنان ابراهیمی ستوده
+Author URI: http://webforest.ir/
+*/
+if (!defined( 'ABSPATH' )) exit;
+require_once('zarinpal.php');
