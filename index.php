c85daf3f977ae99914e80e8be2249f1dcb72da1b
Masoud A <amini141@gmail.com>
2016-10-26 15:59:36 +0330

    fixbug

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 Plugin Name: درگاه زرین پال گرویتی فرم
 Plugin URI: http://gravityforms.ir/
 Description: افزونه درگاه پرداخت زرین پال برای فرم ساز فوق پیشرفته Gravity Forms
-Version: 2.2.0
+Version: 2.2.1
 Author: حنان ابراهیمی ستوده
-Author URI: http://webforest.ir/
+Author URI: http://hannanstd.ir/
 */
 if (!defined('ABSPATH')) exit;
 require_once('zarinpal.php');
