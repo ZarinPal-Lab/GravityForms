7ac5f227a1a0c8dfb97bf089c75eada04d0ff6ed
yousefi1989 <alireza.yousefi98@gmail.com>
2018-06-20 10:40:25 +0430

    update to 2.3.0

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: درگاه زرین گیت گرویتی فرم
+Plugin Name: درگاه زرین گیت (درگاه مستقیم زرین پال) گرویتی فرم
 Plugin URI: http://gravityforms.ir/
-Description: افزونه درگاه پرداخت زرین گیت برای فرم ساز فوق پیشرفته Gravity Forms
-Version: 2.2.1
-Author: حنان ابراهیمی ستوده
-Author URI: http://hannanstd.ir/
+Description: افزونه درگاه پرداخت زرین گیت (درگاه مستقیم زرین پال) برای فرم ساز فوق پیشرفته Gravity Forms
+Version: 2.3.0
+Author: گرویتی فرم پارسی
+Author URI: http://gravityforms.ir/
 */
 if (!defined('ABSPATH')) exit;
 require_once('zaringate.php');
